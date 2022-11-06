1f149ab94955ac90c2a514da77a95afa0d23e6d7
Aleksandr Mingalev <amingalev@gmail.com>
2022-11-06 18:23:17 +0100

    fix: include Path options in 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
-const stream = fs.createReadStream('./01-read-file/text.txt', 'utf-8');
+const path = require('path');
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 
+// console.log(path.join(__dirname), 'text.txt');
 stream.on('data', chunk => console.log(chunk));
 stream.on('error', error => console.log('Error', error.message));
